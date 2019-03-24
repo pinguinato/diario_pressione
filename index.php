42b54730f2e8f52c0bbad64971d47484a5ca8653
Roberto Gianotto <gianottoroberto@gmail.com>
2019-03-24 10:42:40 +0100

    Update index.php
    Prepare layout for dashboard with some data examples in html syntax

--- index.php
+++ index.php
@@ -89,6 +89,70 @@ define('AUTHOR', 'Roberto Gianotto');
                 <!-- Content Row -->
                 <div class="row">
                     
+                   <!-- Begin Page Content -->
+        <div class="container-fluid">
+
+          <!-- Page Heading -->
+          <h1 class="h3 mb-2 text-gray-800">Tables</h1>
+          <p class="mb-4">DataTables is a third party plugin that is used to generate the demo table below. For more information about DataTables, please visit the <a target="_blank" href="https://datatables.net">official DataTables documentation</a>.</p>
+
+          <!-- DataTales Example -->
+          <div class="card shadow mb-4">
+            <div class="card-header py-3">
+              <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+            </div>
+            <div class="card-body">
+              <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                  <thead>
+                    <tr>
+                      <th>Id</th>
+                      <th>Pressione Minima</th>
+                      <th>Pressione Massima</th>
+                      <th>Data Acquisizione</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>1</td>
+                      <td>90</td>
+                      <td>130</td>
+                      <td>2011/04/25</td>
+                    </tr>
+                    <tr>
+                      <td>2</td>
+                      <td>90</td>
+                      <td>130</td>
+                      <td>2011/04/25</td>
+                    </tr>
+                    <tr>
+                      <td>3</td>
+                      <td>90</td>
+                      <td>130</td>
+                      <td>2011/04/25</td>
+                    </tr>
+                    <tr>
+                      <td>4</td>
+                      <td>90</td>
+                      <td>130</td>
+                      <td>2011/04/25</td>
+                    </tr>
+                    <tr>
+                      <td>5</td>
+                      <td>90</td>
+                      <td>130</td>
+                      <td>2011/04/25</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <!-- /.container-fluid -->     
+
+
                 </div>      
 
             </div>
@@ -129,11 +193,15 @@ define('AUTHOR', 'Roberto Gianotto');
 
 <!-- Page level plugins -->
 <script src="vendor/chart.js/Chart.min.js"></script>
+<script src="vendor/datatables/jquery.dataTables.min.js"></script>
+<script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
 
 <!-- Page level custom scripts -->
+<!--
 <script src="js/demo/chart-area-demo.js"></script>
 <script src="js/demo/chart-pie-demo.js"></script>
-
+<script src="js/demo/datatables-demo.js"></script>
+-->
 </body>
 
 </html>
